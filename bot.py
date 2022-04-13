8d606250f9620b068c3f5348d0cd1de46ad8d138
Danial selmipoor <danial9713@gmail.com>
2022-04-13 22:44:15 +0430

    rename bot session

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from utils import administration
 logging.basicConfig(level='INFO')
 
 app = Client(
-    'attacker',
+    'attacker_controller',
     api_id=config('api_id', cast=int),
     api_hash=config('api_hash'),
     bot_token=config('bot_token'),

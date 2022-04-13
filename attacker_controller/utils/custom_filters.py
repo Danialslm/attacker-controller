76358a0223a8aa5708af7bfe68ded0067f9082b8
Danial selmipoor <danial9713@gmail.com>
2022-04-14 03:28:20 +0430

    fix importation

--- attacker_controller/utils/custom_filters.py
+++ attacker_controller/utils/custom_filters.py
@@ -1,12 +1,13 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
+from attacker_controller import MAIN_ADMINS
 from attacker_controller.utils import storage
 
 
 async def admin_filter(_, __, m: Message):
     return (
-            m.from_user.id in storage.MAIN_ADMINS or
+            m.from_user.id in MAIN_ADMINS or
             await storage.get_admins(m.from_user.id)
     )
 

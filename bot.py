e95c2916ccc0030a25da38000036b8d198b94aae
Danial selmipoor <danial9713@gmail.com>
2022-04-14 02:58:19 +0430

    specified a folder for sessions

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from utils import administration
 logging.basicConfig(level='INFO')
 
 app = Client(
-    'attacker_controller',
+    'sessions/attacker_controller',
     api_id=config('api_id', cast=int),
     api_hash=config('api_hash'),
     bot_token=config('bot_token'),

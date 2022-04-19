d29a93a0241d8155a2fb5981f5427dd343f379c9
Danial selmipoor <danial9713@gmail.com>
2022-04-19 21:14:50 +0430

    fix type hinting

--- attacker_controller/utils/storage.py
+++ attacker_controller/utils/storage.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import aioredis
 
@@ -7,7 +7,7 @@ from attacker_controller import REDIS_URL
 redis = aioredis.from_url(REDIS_URL, decode_responses=True)
 
 
-async def add_admin(*users_chat_id: List[int]):
+async def add_admin(*users_chat_id: List[int]) -> int:
     """
     Add the given users chat id to redis cache.
     Return number of added item.
@@ -15,7 +15,7 @@ async def add_admin(*users_chat_id: List[int]):
     return await redis.sadd('admins', *users_chat_id)
 
 
-async def remove_admin(*users_chat_id: List[int]):
+async def remove_admin(*users_chat_id: List[int]) -> int:
     """
     Remove the given users chat id from redis cache.
     Return number of removed item.
@@ -23,7 +23,7 @@ async def remove_admin(*users_chat_id: List[int]):
     return await redis.srem('admins', *users_chat_id)
 
 
-async def get_admins(user_chat_id: Optional = None):
+async def get_admins(user_chat_id: Optional[str] = None) -> Union[bool, set]:
     """
     If `user_chat_id` was provided, return boolean that shows user is admin or not.
 

03ad52b1234a9093cd762f6c9daa3a41634d51e5
danial <danial9713@gmail.com>
2022-07-06 21:33:05 +0430

    bash script for starting the project

---
+++ scripts/start.sh
@@ -0,0 +1,13 @@
+pid=$(ps -ef | grep -v grep | grep attacker_controller | awk '{print $2}')
+
+if [ ! -z $pid ]
+then
+    kill -9 $pid
+fi
+
+PROJECT_PATH=$(dirname $(pwd))
+export PYTHONPATH=$PROJECT_PATH
+
+source $PROJECT_PATH/.venv/bin/activate
+
+python $PROJECT_PATH/attacker_controller

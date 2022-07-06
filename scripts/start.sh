9685ecb8839dacbf3107d912c17ac4aa4df15902
danial <danial9713@gmail.com>
2022-07-06 21:48:16 +0430

    improve getting project directory

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,10 @@
 pid=$(ps -ef | grep -v grep | grep attacker_controller | awk '{print $2}')
 
-if [ ! -z $pid ]
-then
-    kill -9 $pid
+if [ ! -z $pid ]; then
+  kill -9 $pid
 fi
 
-PROJECT_PATH=$(dirname $(pwd))
+PROJECT_PATH=$(dirname $(cd "$(dirname "$0")" && pwd))
 export PYTHONPATH=$PROJECT_PATH
 
 source $PROJECT_PATH/.venv/bin/activate

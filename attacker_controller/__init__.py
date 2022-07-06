3399108946fe572c68c833ec525feb8a8840b6f0
danial <danial9713@gmail.com>
2022-07-06 22:06:41 +0430

    fix environment variable name typo

--- attacker_controller/__init__.py
+++ attacker_controller/__init__.py
@@ -7,8 +7,8 @@ DEBUG = config('debug', cast=bool)
 MAIN_ADMINS = config('main_admins', cast=Csv(cast=int))
 """ Main admins are like normal admins but also can add and remove normal admins. """
 
-REDIS_URL = config('redis_url')
-""" Redis connection url. """
+REDIS_URL = config('redis_uri')
+""" Redis connection uri. """
 
 logging.basicConfig(
     level='INFO' if DEBUG else 'ERROR',
